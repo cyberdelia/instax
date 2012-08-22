8f678c39e5062266f4aefbdbeb013b4eb6ce6249
Timothée Peignier <timothee.peignier@tryphon.org>
2012-08-22 12:07:09 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('LICENSE') as f:
 
 setup(
     name='instax',
-    version='0.1.3',
+    version='0.1.4',
     description='Making celery monitoring easier.',
     author='Timothee Peignier',
     author_email='timothee.peignier@tryphon.org',
